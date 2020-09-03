8f45aed7347446ed6ae18c38440c9cb8ab03a6d3
xcr <52365573@qq.com>
2020-09-03 14:24:00 +0800

    初始化项目

---
+++ src/pages/login/login.vue
@@ -0,0 +1,55 @@
+<template>
+	<view>
+
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+            return {
+                form: {
+                    name: '',
+                    intro: '',
+                    sex: ''
+                },
+                checkboxList: [
+                    {
+                        name: '苹果',
+                        checked: false,
+                        disabled: false
+                    },
+                    {
+                        name: '雪梨',
+                        checked: false,
+                        disabled: false
+                    },
+                    {
+                        name: '柠檬',
+                        checked: false,
+                        disabled: false
+                    }
+                ],
+                radioList: [
+                    {
+                        name: '鲜甜',
+                        disabled: false
+                    },
+                    {
+                        name: '麻辣',
+                        disabled: false
+                    }
+                ],
+                radio: '',
+                switchVal: false
+            };
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style>
+
+</style>
